0ef791f1dd63ab9e0eb4576b7694c8324076ecd5
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2024-10-15 19:11:06 +0700

    master

---
+++ src/Kelompok/Client/App.java
@@ -0,0 +1,29 @@
+package Kelompok.Client;
+
+import java.io.*;
+import java.net.*;
+
+class App {
+  public static void main(String[] args) {
+    try {
+      Socket socket = new Socket("192.168.43.45", 5000);
+
+      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+      BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
+
+      System.out.println("Masukan command untuk eksekusi di remote laptop B:");
+      String command = consoleInput.readLine();
+      out.println(command);
+
+      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      String response;
+      while ((response = in.readLine()) != null) {
+        System.out.println(response);
+      }
+
+      socket.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
